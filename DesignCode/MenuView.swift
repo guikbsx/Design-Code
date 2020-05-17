8bfd3043148b778512b4641d9fc9a3f59e679e70
Guillaume Bisiaux <gbisiaux59@icloud.com>
2020-05-17 17:10:43 +0200

    SF Symbols et Props

---
+++ DesignCode/MenuView.swift
@@ -0,0 +1,53 @@
+//
+//  MenuView.swift
+//  DesignCode
+//
+//  Created by Guillaume Bisiaux on 17/05/2020.
+//  Copyright © 2020 guik development. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                MenuRow(title: "Account", icon: "gear")
+                MenuRow(title: "Billing", icon: "creditcard")
+                MenuRow(title: "Sign out", icon: "person.crop.circle")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(radius: 30)
+            .padding(.horizontal, 30)
+        }
+        .padding(.bottom, 30)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
+
+struct MenuRow: View {
+    
+    var title: String
+    var icon: String
+    
+    var body: some View {
+        HStack(spacing: 16.0) {
+            Image(systemName: icon)
+                .font(.system(size: 20, weight: .light))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+            Text(title)
+                .font(.system(size: 20, weight: .bold))
+                .frame(width: 120, alignment: .leading)
+        }
+    }
+}
