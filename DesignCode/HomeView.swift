21119f91f53b46639cc460888aad2b4fde5d2f88
Guillaume Bisiaux <gbisiaux59@icloud.com>
2020-05-17 18:32:49 +0200

    Binding and Screen Size

---
+++ DesignCode/HomeView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by Guillaume Bisiaux on 17/05/2020.
+//  Copyright © 2020 guik development. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                Spacer()
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
